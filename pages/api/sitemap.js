858a8ea540ba0c9dd45e84ff2ae79f156cf40c22
Gabriel <gabriel.ccacya@tecsup.edu.pe>
2026-06-17 22:24:02 -0500

    V2 LastTry

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,6 +1,5 @@
 const BASE_URL = "https://mi-proyecto-seo-six.vercel.app";
 
-// Simulando datos de una base de datos
 const getPostsFromDB = async () => {
   return [
     { slug: "introduccion-nextjs" },
@@ -15,12 +14,9 @@ export default async function handler(req, res) {
   const dynamicUrls = posts.map((post) => `/blog/${post.slug}`);
   const allUrls = [...staticUrls, ...dynamicUrls];
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${allUrls.map((url) => `<url><loc>${BASE_URL}${url}</loc></url>`).join("")}
-  </urlset>`;
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${allUrls.map((url) => `<url><loc>${BASE_URL}${url}</loc></url>`).join("")}</urlset>`;
 
-  res.setHeader("Content-Type", "text/xml");
+  res.setHeader("Content-Type", "application/xml");
   res.write(sitemap);
   res.end();
 }
