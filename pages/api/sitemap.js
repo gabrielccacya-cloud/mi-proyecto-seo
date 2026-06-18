35a37a26296c5393db35d43d09820c7c9fec9a90
Gabriel <gabriel.ccacya@tecsup.edu.pe>
2026-06-18 00:10:38 -0500

    sitemap con API externa

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,17 +1,13 @@
 const BASE_URL = "https://mi-proyecto-seo-six.vercel.app";
 
-const getPostsFromDB = async () => {
-  return [
-    { slug: "introduccion-nextjs" },
-    { slug: "optimizacion-seo" },
-    { slug: "lazy-loading" },
-  ];
-};
-
 export default async function handler(req, res) {
   const staticUrls = ["/", "/blog", "/contacto"];
-  const posts = await getPostsFromDB();
-  const dynamicUrls = posts.map((post) => `/blog/${post.slug}`);
+
+  // Obteniendo posts desde una API externa (simula un CMS)
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5");
+  const posts = await response.json();
+  const dynamicUrls = posts.map((post) => `/blog/${post.id}`);
+
   const allUrls = [...staticUrls, ...dynamicUrls];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${allUrls.map((url) => `<url><loc>${BASE_URL}${url}</loc></url>`).join("")}</urlset>`;
